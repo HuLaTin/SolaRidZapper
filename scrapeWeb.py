aa52d68fddc7151d21f8043392251df28c5f31f7
Hunter Tiner <Hulatin@gmail.com>
2021-02-24 16:29:51 -0600

    test

--- scrapeWeb.py
+++ scrapeWeb.py
@@ -2,12 +2,14 @@ import requests
 import lxml.html
 from urllib.request import urlopen
 from lxml import etree
+
 loginData = {'mailuid':'rice', 'pwd':'rice','login-submit':''}
 r = requests.post('http://69.4.196.7/solarid/includes/login.inc.php', loginData)
 html = etree.HTML(r.text)
 
 # find = etree.XPath("//table/tr/*[contains(text(),'42')]/text()")
-find = etree.XPath("//table/tr[td/text() = '42']/td/text()")
+find42 = etree.XPath("//table/tr[td[1]/text() = '42']/td/text()")
+find43 = etree.XPath("//table/tr[td[1]/text() = '43']/td/text()")
 
-find(html)
-
+find42(html)
+find43(html)
